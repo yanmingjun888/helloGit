3bd2fbbbe4ccf12b7fe25bcc62797c15aa1f2af9
yanmj888 <ymjit1998@aliyun.com>
2020-11-20 22:05:53 +0800

    添加文件

---
+++ helloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author 闫铭骏 Email:ymjit1998@163.com
+ * @date 2020年11月20日 21:51
+ * @Description
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
